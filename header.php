711dfa37c3b9d40f27ea0d3512e7f0d851fb7889
sheff4uk <sheff4uk@gmail.com>
2018-09-14 13:21:35 +0300

    Яндекс метрика

--- header.php
+++ header.php
@@ -12,6 +12,36 @@
 	<script src="/js/jquery-1.11.3.min.js"></script>
 	<script src="/js/jquery.bxslider.js"></script>
 	<script src="/js/script.js?v=1"></script>
+
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+		(function (d, w, c) {
+			(w[c] = w[c] || []).push(function() {
+				try {
+					w.yaCounter50341759 = new Ya.Metrika2({
+						id:50341759,
+						clickmap:true,
+						trackLinks:true,
+						accurateTrackBounce:true,
+						webvisor:true
+					});
+				} catch(e) { }
+			});
+
+			var n = d.getElementsByTagName("script")[0],
+				s = d.createElement("script"),
+				f = function () { n.parentNode.insertBefore(s, n); };
+			s.type = "text/javascript";
+			s.async = true;
+			s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+			if (w.opera == "[object Opera]") {
+				d.addEventListener("DOMContentLoaded", f, false);
+			} else { f(); }
+		})(document, window, "yandex_metrika_callbacks2");
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/50341759" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
 </head>
 
 <body>
