d0ec626fd99a1f6be5643c83b5a63f4ccead20f1
sheff4uk <sheff4uk@gmail.com>
2018-10-12 16:06:23 +0300

    fix

--- contact.php
+++ contact.php
@@ -18,10 +18,10 @@
 			<p>Email: <a href="mailto:fabrikaprestol@gmail.com" target="_blank">fabrikaprestol@gmail.com</a></p>
 		</section>
 	</section>
-	<div style="height: 400px; margin-bottom: -40px; text-align: center;">
-		<h1 style="position: absolute; width: 100%;">Схема проезда</h1>
+<!--	<div style="height: 400px; margin-bottom: -40px; text-align: center;">-->
+<!--		<h1 style="position: absolute; width: 100%;">Схема проезда</h1>-->
 		<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A4413ac25e1740ad9845b18913f51b05d394299e7a1cf0112f081b405ef0d82bc&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
-	</div>
+<!--	</div>-->
 </section>
 
 <?
