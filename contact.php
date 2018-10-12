0207ddfdd7919e8d8a254a002417eac97d48b496
sheff4uk <sheff4uk@gmail.com>
2018-10-12 15:13:05 +0300

    Почта и телефон кликабельные

--- contact.php
+++ contact.php
@@ -14,11 +14,12 @@
 		<h1>Контакты</h1>
 		<section class="contact1">
 			<p>Адрес производства: г.Киров, ул.Луганская 59в</p>
-			<p>Телефон: 8(909)131-77-32</p>
-			<p>email: fabrikaprestol@gmail.com</p>
+			<p>Телефон: <a href="tel:89091317732">8 (909) 131-77-32</a></p>
+			<p>Email: <a href="mailto:fabrikaprestol@gmail.com" target="_blank">fabrikaprestol@gmail.com</a></p>
 		</section>
 	</section>
-	<div style="height: 400px; margin-bottom: -40px;">
+	<div style="height: 400px; margin-bottom: -40px; text-align: center;">
+		<h1 style="position: absolute; width: 100%;">Схема проезда</h1>
 		<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A4413ac25e1740ad9845b18913f51b05d394299e7a1cf0112f081b405ef0d82bc&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
 	</div>
 </section>
