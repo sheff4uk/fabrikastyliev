4412ca892412db905d51028373f7ed68d51e5080
sheff4uk <sheff4uk@gmail.com>
2019-03-20 21:26:30 +0300

    Увеличил размер текста

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 	<meta name="keywords" content="мебель, журнальные столы, столы, стулья, стул, стол, мебель для дома, престол, фабрикастульев, журнальный стол, кресла, журнальный столик, кухонный стол, стол на кухню">
 	<link rel="icon" href="favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-	<link type="text/css" rel="stylesheet" href="/css/style.css?v=19">
+	<link type="text/css" rel="stylesheet" href="/css/style.css?v=20">
 	<link rel='stylesheet' type='text/css' href='css/loading.css'>
 <!--	<link type="text/css" rel="stylesheet" href="/css/jquery.bxslider.css">-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
