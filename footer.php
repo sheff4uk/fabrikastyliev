ea393028187e1b1beff038f5df9142e1ddb8fc1d
sheff4uk <sheff4uk@gmail.com>
2019-11-20 21:32:11 +0300

    fix

--- footer.php
+++ footer.php
@@ -34,8 +34,6 @@
 	</div>
 </footer>
 
-<!--<script type="text/javascript" src="https://vk.com/js/api/openapi.js?159"></script>-->
-
 <!-- VK Widget -->
 <div id="vk_community_messages"></div>
 <script type="text/javascript">
