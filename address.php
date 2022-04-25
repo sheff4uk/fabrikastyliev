45fdba2a61fd9527764b5b585b0a44bc6b26a4e4
sheff4uk <sheff4uk@gmail.com>
2022-04-25 13:56:46 +0300

    Изменились телефоны салонов

--- address.php
+++ address.php
@@ -31,19 +31,19 @@
 			</h4>
 			<ul>
 				<li class="shop" id="materik">
-					ТЦ "Открытый Материк" - ул. Ларина, 7 - т. 8(999)076-17-55 <a href="#materik" class="map_btn" title="Посмотреть на карте"><i class="fas fa-map-marked-alt"></i> Карта</a>
+					ТЦ "Открытый Материк" - ул. Ларина, 7 <a href="tel:+79990761755">+7 999 076 17 55</a> <a href="#materik" class="map_btn" title="Посмотреть на карте"><i class="fas fa-map-marked-alt"></i> Карта</a>
 					<div class="map">
 						<div style="height: 400px;"><iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=111126599301" width="100%" height="400" frameborder="0"></iframe></div>
 					</div>
 				</li>
 				<li class="shop" id="bazar">
-					ТЦ «Мебельный базар» - ул. Гордеевская, 7 - т. 8(920)072-20-67 <a href="#bazar" class="map_btn" title="Посмотреть на карте"><i class="fas fa-map-marked-alt"></i> Карта</a>
+					ТЦ «Мебельный базар» - ул. Гордеевская, 7 <a href="tel:+79697621587">+7 969 762 15 87</a> <a href="#bazar" class="map_btn" title="Посмотреть на карте"><i class="fas fa-map-marked-alt"></i> Карта</a>
 					<div class="map">
 						<div style="height: 400px;"><iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=1169209411" width="100%" height="400" frameborder="0"></iframe></div>
 					</div>
 				</li>
 				<li class="shop" id="bum">
-					ТЦ «БУМ» - ул. Бекетова, 13к - т. 8(960)195-02-85 <a href="#bum" class="map_btn" title="Посмотреть на карте"><i class="fas fa-map-marked-alt"></i> Карта</a>
+					ТЦ «БУМ» - ул. Бекетова, 13к <a href="tel:+79990760946">+7 999 076 09 46</a> <a href="#bum" class="map_btn" title="Посмотреть на карте"><i class="fas fa-map-marked-alt"></i> Карта</a>
 					<div class="map">
 						<div style="height: 400px;"><iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=187931418140" width="100%" height="400" frameborder="0"></iframe></div>
 					</div>
@@ -52,7 +52,7 @@
 			<h4>Сыктывкар</h4>
 			<ul>
 				<li class="shop" id="gigamart">
-					ТЦ «ГигаМарт» - ул. Чкалова, 38 - т. 8(904)221-30-55 <a href="#gigamart" class="map_btn" title="Посмотреть на карте"><i class="fas fa-map-marked-alt"></i> Карта</a>
+					ТЦ «ГигаМарт» - ул. Чкалова, 38 <a href="tel:+79042213055">+7 904 221 30 55</a> <a href="#gigamart" class="map_btn" title="Посмотреть на карте"><i class="fas fa-map-marked-alt"></i> Карта</a>
 					<div class="map">
 						<div style="height: 400px;"><iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=1223313421" width="100%" height="400" frameborder="0"></iframe></div>
 					</div>
